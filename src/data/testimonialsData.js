d9cb8514467c23eaf3895f092cb4c55ce1f1288a
Rahul <rahulzandennavar99@gmail.com>
2023-07-17 16:25:12 +0530

    css optimized

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -7,18 +7,18 @@ export const testimonialsData = [
     image: image1,
     review:
       "The fitness coach at Fitclub provided expert guidance and support, leading you to achieve your ideal body through the right plan and program.",
-    name: 'RAHUL ZANDENNAVAR',
+    name : 'RAHUL ZANDENNAVAR',
     status : 'COACH'
   },
   {
     image: image2,
-    review: 'The fitness coach at Fitclub was exceptional, providing knowledgeable guidance and motivating support that helped me achieve my fitness goals. Their expertise and dedication made all the difference in my journey. ',
+    review:'The fitness coach at Fitclub was exceptional, providing knowledgeable guidance and motivating support that helped me achieve my fitness goals.Their expertise and dedication made all the difference in my journey. ',
     name: 'SATISH KOKANE',
     status: 'CLIENT'
   },
   {
     image : image3,
-    review:'One-to-one training offers a personalized fitness experience tailored to your specific goals and needs, allowing for individualized attention and maximum results. With dedicated focus and customized guidance, you can optimize your workouts and make significant progress towards your fitness aspirations.',
+    review:'One-to-one training offers a personalized fitness experience tailored to your specific goals and needs, allowing for individualized attention and maximum results.With dedicated focus and customized guidance, you can optimize your workouts and make significant progress towards your fitness aspirations.',
     name: 'SHAAN AND SWAGAT',
     status: "CLIENTS"
   }
